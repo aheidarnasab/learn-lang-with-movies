70b3ec2b863c3888fcb2a7b6b1d3942f0b921779
Abbas Heidarnasab <a.heidarnasab@gmail.com>
2018-11-26 11:12:35 +0330

    gitignore file added

---
+++ main.py
@@ -0,0 +1,8 @@
+from read_sub import read_subtitle
+
+addr = r"J:\Movies\# Series\Friends\S01\friends--first-season_HI_english-842737\friends.s01e01.720p.bluray.x264-psychd.srt"
+
+
+
+
+print(read_subtitle(addr))
--- read_sub.py
+++ read_sub.py
@@ -1,12 +1,11 @@
-addr = r"F:\Movies\# Series\Friends\S01\friends--first-season_HI_english-842737\friends.s01e01.720p.bluray.x264-psychd.srt"
-
-f = open(addr)
-
-f.readline()  #some weird characters around 1st line indicator
-for line in f:
-    words = line.split()
-    if len(words) == 1:
-        try:
-            print(int(words[0]))
-        except:
-            pass
+def read_subtitle(addr):
+    f = open(addr)
+    f.readline()  #some weird characters around 1st line indicator
+    for line in f:
+        words = line.split()
+        if len(words) == 1:
+            try:
+                print(int(words[0]))
+            except:
+                pass
+    return "file read"
