35b5abd4e93195c3c22575184aada2d3a7b90a29
Abbas Heidarnasab <a.heidarnasab@gmail.com>
2018-11-26 18:11:55 +0330

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from read_sub import read_subtitle
+from make_dict import mkdict
 
-addr = r"J:\Movies\# Series\Friends\S01\friends--first-season_HI_english-842737\friends.s01e01.720p.bluray.x264-psychd.srt"
+sub_addr = r"J:\Movies\# Series\Friends\S01\friends--first-season_HI_english-842737\friends.s01e01.720p.bluray.x264-psychd.srt"
+dict_addr = r"G:\lang-with-movies\Dict\504.txt"
 
+dict = mkdict(dict_addr)
 
+new = read_subtitle(sub_addr, dict)
 
-
-print(read_subtitle(addr))
+print("end")
